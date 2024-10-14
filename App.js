8721362ea259da75a950d0dd8e4656b3f34a4498
Shyamala21674 <thisshyamala@gmail.com>
2024-10-14 21:42:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <div></div>
+    <div className="App">
+      Hello
+      
+
+      <h1>Information</h1>
+      <label>Name</label>
+      <input type="text"></input>
+      <h1></h1>
+      <label>Email</label>
+      
+      <input type="text"></input>
+      <h1></h1>
+      <input type="button" value="Submit"></input>
+      </div>
+      <div></div>
+      </div>
+  );
+}
+
+export default App;
